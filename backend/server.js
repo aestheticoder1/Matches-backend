650626675c63fb661c7f0008b68304bf3dadf5d7
Nihal Rawat <nihal.riya03@gmail.com>
2025-05-30 23:15:15 +0530

    Bug fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const API_KEY = process.env.FOOTBALL_DATA_API_KEY;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://matches-frontend-eight.vercel.app/"
+}));
 
 // Get all available leagues (competitions)
 app.get('/leagues', async (req, res) => {
